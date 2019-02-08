2d7372093ef093e7a9befdc761279fbc6752ce21
diego costa <diegocosta@MacBook-Air-de-diego.local>
2019-02-07 22:34:44 -0200

    Modelagem de classes e ForeignKey

--- apps/documento/models.py
+++ apps/documento/models.py
@@ -1,8 +1,11 @@
 from django.db import models
+from apps.funcionario.models import Funcionario
 
 
 class Documento(models.Model):
-    nome = models.CharField(max_length=50)
-    
+    descricao = models.CharField(max_length=100)
+    pertence = models.ForeignKey(
+        Funcionario, on_delete=models.PROTECT, null=True, blank=True, default=None)
+
     def __str__(self):
-        return self.nome
+        return self.descricao
--- apps/empresa/models.py
+++ apps/empresa/models.py
@@ -6,4 +6,6 @@ class Empresa(models.Model):
 
     def __str__(self):
         return self.nome
+
+    
     

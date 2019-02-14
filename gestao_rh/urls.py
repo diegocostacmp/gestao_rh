69db3c9e9282ba447aaab07c9de10e45050b2737
diego costa <diegocosta@MacBook-Air-de-diego.local>
2019-02-14 21:35:55 -0200

    Autenticacao de login e logout com Django auth

--- gestao_rh/urls.py
+++ gestao_rh/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', include('apps.core.urls')),
     path('funcionario/', include('apps.funcionario.urls')),
     path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]

59a247d4213c1cc0ad249ae8d99f4c305d2699e5
diego costa <diegocosta@MacBook-Air-de-diego.local>
2019-02-18 21:14:09 -0300

    Cadastro de empresa do funcionario

--- apps/empresa/views.py
+++ apps/empresa/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
 from django.views.generic.edit import CreateView
 from .models import  Empresa
+from django.http import HttpResponse
 
 class EmpresaCreate(CreateView):
     model = Empresa
-    fields = ['nome', ]
+    fields = ['nome']
+
+    def form_valid(self, form):
+        obj = form.save()
+        funcionario = self.request.user.funcionario
+        funcionario.empresa = obj
+        funcionario.save()
+
+        return HttpResponse('ok')
 

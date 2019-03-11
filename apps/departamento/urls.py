02ebcce73e7bfc773ad4ca93899f5faf6b94a56b
Diego Costa <diegocostacmp@gmail.com>
2019-03-11 20:43:00 -0300

    Update departamento

--- apps/departamento/urls.py
+++ apps/departamento/urls.py
@@ -2,13 +2,14 @@ from django.urls import path
 from .views import (
     DepartamentoList,
     DepartamentoCreate,
+    DepartamentoUpdate,
 
 )
 
 urlpatterns = [
     path('list', DepartamentoList.as_view(), name='list_departamento'),
     path('novo/', DepartamentoCreate.as_view(), name='create_departamento'),
+    path('editar/<int:pk>/', DepartamentoUpdate.as_view(), name='update_departamento'),
     # path('', FuncionarioList.as_view(), name='list_funcionario'),
-    # path('editar/<int:pk>/', FuncionarioEdit.as_view(), name='update_funcionario'),
     # path('deletar/<int:pk>/', FuncionarioDelete.as_view(), name='delete_funcionario'),
 ]
--- apps/departamento/views.py
+++ apps/departamento/views.py
@@ -1,5 +1,4 @@
-
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, CreateView, UpdateView
 from .models import Departamento
 
 
@@ -20,3 +19,7 @@ class DepartamentoCreate(CreateView):
         departamento.empresa = self.request.user.funcionario.empresa
         departamento.save()
         return super(DepartamentoCreate, self).form_valid(form)
+
+class DepartamentoUpdate(UpdateView):
+    model = Departamento
+    fields = ['nome']

5466ae0f6c46fb844e29ff9e04d04a08b77d5e7f
Diego Costa <diegocostacmp@gmail.com>
2019-06-22 18:20:29 -0300

    Update view hora extra funcionario

--- apps/hora_extra/urls.py
+++ apps/hora_extra/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 from .views import (
     HoraExtraList,
-    # FuncionarioEdit,
+    HoraExtraEdit
     # FuncionarioDelete, 
     # FuncionarioCreate
     )
@@ -9,7 +9,7 @@ from .views import (
 urlpatterns = [
     path('', HoraExtraList.as_view(), name='list_hora_extra'),
     # path('novo/', FuncionarioCreate.as_view(), name='create_funcionario'),
-    # path('editar/<int:pk>/', FuncionarioEdit.as_view(), name='update_funcionario'),
+    path('editar/<int:pk>/', HoraExtraEdit.as_view(), name='update_hora_extra'),
     # path('deletar/<int:pk>/', FuncionarioDelete.as_view(), name='delete_funcionario'),
     
 ]
--- apps/hora_extra/views.py
+++ apps/hora_extra/views.py
@@ -14,3 +14,7 @@ class HoraExtraList(ListView):
     def get_queryset(self):
         empresa_logada = self.request.user.funcionario.empresa
         return HoraExtra.objects.filter(funcionario__empresa=empresa_logada)
+
+class HoraExtraEdit(UpdateView):
+    model = HoraExtra
+    fields = ['motivo', 'funcionario', 'horas']   
